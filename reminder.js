c6d3ceb1d419810076598bdace5c1b7e570fae32
ZilchofNowhere <81156765+ZilchofNowhere@users.noreply.github.com>
2021-07-07 16:04:48 +0300

    A program to remind you of the things that you should do

--- reminder.js
+++ reminder.js
@@ -4,10 +4,13 @@ const rl = readline.createInterface({input: process.stdin, output: process.stdou
 rl.question("Lütfen hatırlatılmak istediğiniz şeyi girin\n", (answer)=>{
    console.log(`${answer} için kurduğunuz hatırlatıcı başlıyor...`)
    rl.question("Ne kadar süre sonra hatırlatılacağını girin (dakika cinsinden)\n", (dur)=>{
-      console.log(`Hatırlatmanız ${dur} dakika sonra gerçeklecek`)
+      console.log(`Hatırlatmanız ${dur} dakika sonra gerçekleşecek`)
       setTimeout(() => {
-         console.log(`${answer} zamanı!!!`) 
-      }, parseInt(dur)*60000); // 1 dk = 60000 ms
+         console.log(`${answer} zamanı!!!`)
+         setTimeout(()=>{
+            console.log("Görüşmek üzere")
+         }, 2000) 
+      }, parseFloat(dur)*60000); // 1 dk = 60000 ms
       clearTimeout()
       if (clearTimeout) {
          rl.close()
